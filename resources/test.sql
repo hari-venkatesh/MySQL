898c92a890bda436adb705adbac7d32ea4546cba
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-07-17 19:27:24 +0530

    Added test.sql

---
+++ resources/test.sql
@@ -0,0 +1,11 @@
+use sql_inventory;
+
+SELECT *
+FROM products
+Where product_id
+between 3 and 10;
+
+-- So idhula irundhe save pannikkraya?_inum plan pannala, course muduchuta idhu panuradhu ella 
+ -- half hour, so ona panikalanu vituta_Once nee andha close button ah click pantina, sql queries
+ -- poirum. So parallel ah save panna mudinja pannikkonga.
+ -- ctrl + s inga irundhu kuduthale save pannikalaam
