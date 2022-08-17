fa3f87dffb81fe8f16d24e61b0de40a3390a2d76
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-08-17 21:57:23 +0530

    New Excerice Started

---
+++ Practice2/CreatingDatabase&Tables.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE exercise2;
+
+CREATE TABLE exercise2.customer (
+	cus_id INT(10)
+	,first_name VARCHAR(20) NOT NULL
+	,last_nmae VARCHAR(20)
+	,prod_id INT(10)
+	,CONSTRAINT prod_id_check
+		CHECK(LENGTH(prod_id)=4)
+	,CONSTRAINT prime_c
+		PRIMARY KEY(cus_id)
+	,CONSTRAINT forn_k
+		FOREIGN KEY(prod_id)
+			REFERENCES exercise2.product(prod_id)
+			ON DELETE CASCADE
+);
+
+
+CREATE TABLE exercise2.product (
+	prod_id INT(10)
+	,prod_name VARCHAR(20) NOT NULL
+	,prod_price INT(20)
+	,CONSTRAINT prime_p
+	PRIMARY KEY (prod_id)
+);
+
+
