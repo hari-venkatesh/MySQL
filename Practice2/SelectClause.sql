0a4f1310a0c9f724324d8cb4f7f191af7daa96d6
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-08-19 22:13:06 +0530

    Query to Fetch Records

---
+++ Practice2/SelectClause.sql
@@ -0,0 +1,32 @@
+
+SELECT cus_id
+	, first_name
+    , last_nmae
+    , prod_id
+    , email_id
+    , no_of_purchase
+    , dob
+    , age
+FROM exercise2.customer;
+
+-----------------------------------------------------------------------
+
+SELECT cus_id
+	, first_name
+	, last_nmae
+	, prod_id
+	, email_id
+FROM exercise2.customer;
+    
+------------------------------------------------------------------------
+
+SELECT prod_id
+	, prod_name
+    , prod_price
+FROM exercise2.product;
+
+------------------------------------------------------------------------
+
+SELECT prod_id
+FROM exercise2.product
+
