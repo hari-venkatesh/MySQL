f9bf4f695a22ceedbe6ec09204343c2443659967
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-08-22 21:25:05 +0530

    Huge Amount Of Insertion

---
+++ Practice3/InsertingRecords.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO exercise3.university (univ_code, university_name)
+VALUES (7113, 'KPR');
+
+INSERT INTO exercise3.university (univ_code, university_name)
+VALUES (7114, 'KKR');
+
+INSERT INTO exercise3.university (univ_code, university_name)
+VALUES (7115, 'PPR');
+
+-----------------------------------------------------------------------
+
+INSERT INTO exercise3.department (dept_code, dept_name, univ_code)
+VALUES (123, 'CSE', 7113);
+
+INSERT INTO exercise3.department (dept_code, dept_name, univ_code)
+VALUES (124, 'ECE', 7113);
+
+INSERT INTO exercise3.department (dept_code, dept_name, univ_code)
+VALUES (125, 'MECH', 7113);
+
+INSERT INTO exercise3.department (dept_code, dept_name, univ_code)
+VALUES (126, 'IT', 7113);
+
+-----------------------------------------------------------------------
+
+-- and soo on for every table.....................
